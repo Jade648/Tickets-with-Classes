06608a95747aa9f672742e632d04cc321be1efbe
Jade645 <maddiepeterson375@gmail.com>
2022-09-23 15:02:35 -0500

    Started the Ticket system assingment

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+ using System.IO;
+ using System.Collections.Generic;
+ using System.Linq;
+ 
+namespace Tickets_with_Classes
+{
+    public class Tickets
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
