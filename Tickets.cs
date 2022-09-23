be2d837b87ee964d9d961a08405680b00cd0da03
Jade645 <maddiepeterson375@gmail.com>
2022-09-23 17:30:06 -0500

    Updated Program file

--- Tickets.cs
+++ Tickets.cs
@@ -12,13 +12,14 @@ namespace Tickets_with_Classes
         
         static void Main(string[] args){
 
+            logger.Info("Program started");
+
             string file ="Tickets.csv";
 
             string choice = "";
 
         do{
-        logger.Info("Program started");
-        
+
             Console.WriteLine("1)Read data from file");
             Console.WriteLine("2)Create file from data");
             Console.WriteLine("3) Press any key to exit program");
@@ -40,8 +41,14 @@ namespace Tickets_with_Classes
                             string [] arr = line.Split('|');
 
                             Console.WriteLine("choice: {1},choice {2}", arr [1],arr[2]);
+                            } 
+                             sr.Close();
+                            
+                            }else {
 
-                            if (choice == "2"){
+           Console.WriteLine("File does not exist");
+        }  
+             if (choice == "2"){
 
         StreamWriter sw = new StreamWriter(file,append:true);
         for(int i =0; i < 7; i++) {
@@ -76,22 +83,9 @@ namespace Tickets_with_Classes
                sw.WriteLine(status);
 
             }
-            
                sw.Close(); 
-
             }
-        
-            while (choice == "1"|choice == "2");
-        }else {
+        } while (choice == "1" |choice == "2"){
 
-           Console.WriteLine("File does not exist");
-
-        }  
-            sr.Close();
-                    } 
-
-          logger.Info("Program ended");
-        } 
-    } 
-
+            logger.Info("Program ended");
 
