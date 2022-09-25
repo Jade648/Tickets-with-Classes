c7bdd6aa06220b9d8854952ffbe56217c31b1b00
Jade645 <maddiepeterson375@gmail.com>
2022-09-25 16:48:10 -0500

    Added Ticket Object to Main

--- Tickets.cs
+++ Tickets.cs
@@ -63,6 +63,8 @@ namespace Tickets_with_Classes
                 break; 
                }
 
+               Tickets Ticket = new Ticket; 
+
                Console.WriteLine("Enter the name of who submitted the ticket");
 
                string name = Console.ReadLine().ToUpper();
@@ -88,4 +90,7 @@ namespace Tickets_with_Classes
         } while (choice == "1" |choice == "2"){
 
             logger.Info("Program ended");
+        }
+    } 
+
 
