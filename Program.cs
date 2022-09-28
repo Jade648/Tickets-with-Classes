a8128311e6b0cc2545733ac8f1207dee75b701f2
Jade645 <maddiepeterson375@gmail.com>
2022-09-28 17:30:07 -0500

    updated main file

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Ticket
 {
     public class Program
     {
+        private const string V = "Tickets.csv";
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
         static void Main(string[] args)
@@ -15,14 +16,16 @@ namespace Ticket
 
             logger.Info("Program started");
 
-            string file ="Tickets.csv";
+             Tickets ticket  = new Tickets ("\\Tickets.csv");
+
+            string file = "\\Tickets.csv";
 
             string choice = "";
 
         do{
 
-            Console.WriteLine("1)Read data from file");
-            Console.WriteLine("2)Create Ticket file from data");
+            Console.WriteLine("1) Read data from file");
+            Console.WriteLine("2) Create file from data");
             Console.WriteLine("3) Press any key to exit program");
 
             choice = Console.ReadLine();
@@ -30,64 +33,84 @@ namespace Ticket
             logger.Info("User choice:{choice}",choice);
         {
            if (choice == "1"){
-                
+
                 if (File.Exists(file)){
                          
-                         StreamReader sr = new StreamReader(file);
+                  StreamReader sr = new StreamReader(file);
 
-                         while (!sr.EndOfStream){
+                    sr.ReadLine();
 
-                            string[] arr = File.ReadAllLines("source\\Tickets.csv");
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                    }
+                    sr.Close();
+                
 
-                            Console.WriteLine("choice: {1},choice {2}", arr [1],arr[2]);
-                            } 
-                             sr.Close();
+                } else if(choice == "2"){
 
-                            } else if (choice == "2"){
+               try {StreamWriter sw = new StreamWriter(file,true);
 
-                        try { StreamWriter sw = new StreamWriter(file,append:true);
-                 {
               Console.WriteLine("Enter a Ticket (Y/N)?");
 
-              string resp = Console.ReadLine().ToUpper();
+               Console.ReadLine();{
 
-               if (resp != "Y") 
-               {
-                break; 
+              break;
                }
-                 
+               
+               foreach(Tickets :: Ticket = "//Tickets.csv");
+
+                    {
+                        Console.WriteLine(ticket);
+                    }
+
                Console.WriteLine("Enter the name of who submitted the ticket");
 
                string name = Console.ReadLine().ToUpper();
 
-               sw.WriteLine (name);
-
               Console.WriteLine("Enter the ticket's description");
 
               string description = Console.ReadLine().ToUpper();
-
-              sw.WriteLine ( description);
                
                Console.WriteLine("Enter Ticket's Status");
 
                string status = Console.ReadLine().ToUpper();
 
-               sw.WriteLine(status);
-                 }
+               Console.ReadLine().ToUpper();
 
-                  sw.Close(); 
-            } catch (IndexOutOfRangeException ex){
+               sw.WriteLine($"{name},{description},{status}");
+
+               } catch (IndexOutOfRangeException ex){
 
                Console.WriteLine("Index  was out of Range");
+               
+              } logger.Info ("caught index out of range exception");    
 
-              } logger.Info ("caught index out of range exception");   
-            }  
+              } else {
+
+              Console.WriteLine("File can not be found");
+
+                          logger.Error("File does not exist: {File}", file);          
+            }        
           } 
-        }
-      } while(choice == "1"|choice == "2");
-    } 
+        } 
+        } while(choice == "1"|| choice == "2"); 
+
+          logger.Info("program ended");
+      } 
+    }
   }
-}
+
+  
+
+
+    
+
+
+    
+
+       
+    
   
 
 
