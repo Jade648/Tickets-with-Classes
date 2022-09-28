488269e27fd5e8f5b9d27a9d404a8f744e9684d0
Jade645 <maddiepeterson375@gmail.com>
2022-09-28 10:48:34 -0500

    Finished app

---
+++ Tickets.cs
@@ -0,0 +1,41 @@
+namespace Ticket
+{
+    internal class Tickets
+    {
+        string name;
+
+    string description;
+
+    string status;
+
+    public Tickets(string name, string description, string status) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public string getDescription() {
+        return description;
+    }
+
+    public void setDescription(string description) {
+        this.description = description;
+    }
+
+    public string getStatus() {
+        return status;
+    }
+
+    public void setStatus(string status) {
+        this.status = status;
+    }
+}
+    }
